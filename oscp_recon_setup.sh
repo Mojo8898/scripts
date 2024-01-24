ecc4f5ccdcfd33d37146a286429985f8c4c92ac2
Mojo8898 <zmewshaw@gmail.com>
2024-01-24 14:04:16 -0500

    Updated

--- oscp_recon_setup.sh
+++ oscp_recon_setup.sh
@@ -22,9 +22,15 @@ tmux rename-window -t $session:0 "openvpn"
 
 # Create new window to work in
 tmux new-window -t $session
+tmux send-keys -t $session:1 "clear" C-m
+tmux send-keys -t $session:1 "sleep .2" C-m
+tmux send-keys -t $session:1 "clear" C-m
 
 # Create a new pane to dynamically view recommended commands based on nmap output
 tmux split-window -h -t $session:1
+tmux send-keys -t $session:1 "clear" C-m
+tmux send-keys -t $session:1 "sleep .2" C-m
+tmux send-keys -t $session:1 "clear" C-m
 
 # Attach to the tmux session
 tmux attach -t $session

